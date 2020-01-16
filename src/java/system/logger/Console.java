54517d37704d67ee9d145f3a5cdef6643f501368
HalfScale3 <HalfScale3@gmail.com>
2020-01-16 11:30:46 +0800

    User Module Update 2020-01-16

--- src/java/system/logger/Console.java
+++ src/java/system/logger/Console.java
@@ -5,16 +5,21 @@
  */
 package system.logger;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 /**
  *
  * @author MacMuffin
  */
 public class Console {
+
     private final static Logger logger = LogManager.getLogger(Console.class);
-    
-    public static void log(String message) {
-        logger.debug(message);
+
+    public static void log(String... message) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(StringUtils.join(message, " "));
+        logger.debug(sb);
     }
 }
--- web/system/user/assets/js/user.js
+++ web/system/user/assets/js/user.js
@@ -16,6 +16,7 @@ $(function () {
                     return data.firstName + ' ' + data.lastName;
                 }
             },
+            {data: 'username'},
             {data: 'email'},
             {data: 'status'}
         ],
@@ -31,6 +32,7 @@ $(function () {
     
     userTable.on('click', '.userRow', function() {
         var data = $(this).data('user.row.data');
+        
         userUpdateModal.fillForm({
             source: data,
             filter: function(key, val, elem) {
@@ -48,31 +50,60 @@ $(function () {
 
     userAddModal.standardDialog({
         title: 'Add User',
+        scrollable: true,
         ajax: function (fd) {
+            
+            if (fd.password !== fd.confirmPassword) {
+                return {status: 1, response: 'Password does not match!'};
+            }
+            
             return $.post($g.root_path + 'user.create', fd);
         },
         done: function (result, modal) {
             userTable.DataTable().ajax.reload();
-            modal.modal('hide');
-            sysAlert({
-                text: result.response,
-                delay: 2000
-            });
+            if (result.status === 0) {
+                sysAlert({
+                    text: result.response,
+                    delay: 2000
+                });
+                modal.modal('hide');
+            }else if (result.status === 1) {
+                sysAlert({
+                    text: result.response,
+                    delay: 4000,
+                    type: 'danger'
+                });
+            }
         }
     });
     
     userUpdateModal.standardDialog({
         title: 'Update User',
+        scrollable: true,
         ajax: function (fd) {
+            console.log('fd', fd);
+            if (fd.password !== fd.confirmPassword) {
+                return {status: 1, response: 'Password does not match!'};
+            }
+            
             return $.post($g.root_path + 'user.update', fd);
         },
         done: function (result, modal) {
             userTable.DataTable().ajax.reload();
-            modal.modal('hide');
-            sysAlert({
-                text: result.response,
-                delay: 2000
-            });
+            
+            if (result.status === 0) {
+                sysAlert({
+                    text: result.response,
+                    delay: 2000
+                });
+                modal.modal('hide');
+            } else if (result.status === 1) {
+                sysAlert({
+                    text: result.response,
+                    delay: 4000,
+                    type: 'danger'
+                });
+            }
         }
     });
 });
